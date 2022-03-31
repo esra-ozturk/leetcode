b514162a9f03f0f004095995ef392c9c141124ca
esra-ozturk <esrakeklik24@gmail.com>
2022-03-31 16:22:48 +0200

    code editing

--- trips_and_users.sql
+++ trips_and_users.sql
@@ -1,8 +1,8 @@
-select 
+SELECT 
 t.Request_at Day, 
-round(sum(case when t.status = 'completed' then 0 else 1 end) / count(id), 2) 'Cancellation Rate'
-from trips t
-where t.request_at between '2013-10-01' and '2013-10-03'
+round(sum(CASE WHEN t.status = 'completed' THEN 0 ELSE 1 END) / count(id), 2) 'Cancellation Rate'
+FROM trips t
+where t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
 AND t.client_id IN (SELECT u.users_id FROM users u WHERE u.banned= 'No' )
 AND t.driver_id  IN (SELECT u.users_id FROM users u WHERE u.banned= 'No' )
-group by t.request_at
+GROUP BY  t.request_at
